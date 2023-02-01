6c0abb64aeed6ee973634bfbadf1aef409d07480
moldir <moldirshb@gmail.com>
2023-02-01 13:26:13 -0600

    logout

--- starter/src/components/Header.js
+++ starter/src/components/Header.js
@@ -14,41 +14,57 @@ const Header = () => {
     }
 
     return (
-        <header className='header flex-row'>
-            <div className='flex-row'>
-                <img src={logo} alt='dm-logo' className='logo'/>
-                <h2>Social Mountain</h2>
-            </div>
-            <nav>
-                {authCtx.token? ( <ul className='main-nav'>
-                    <li>
-                        <NavLink style={styleActiveLink} to='/'>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink style={styleActiveLink} to='profile'>Profile</NavLink>
-                    </li>
-                    <li>
-                        <NavLink style={styleActiveLink} to='form'>Add Post</NavLink>
-                    </li>
-                    <li>
-                        <NavLink style={styleActiveLink} to='auth'>Login or Register</NavLink>
-                    </li>
-                </ul>)
-                    :
-                    (<ul className='main-nav'>
-                    <li>
-                        <NavLink style={styleActiveLink} to='/'>Home</NavLink>
-                        </li>
-                    
-                       <li>
-                        <NavLink style={styleActiveLink} to='auth'>Login or Register</NavLink>
-                    </li>
-                </ul>
-                    )}
-               
-            </nav>
-        </header>
-    )
+      <header className="header flex-row">
+        <div className="flex-row">
+          <img src={logo} alt="dm-logo" className="logo" />
+          <h2>Social Mountain</h2>
+        </div>
+        <nav>
+          {authCtx.token ? (
+            <ul className="main-nav">
+              <li>
+                <NavLink style={styleActiveLink} to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={styleActiveLink} to="profile">
+                  Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={styleActiveLink} to="form">
+                  Add Post
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  style={styleActiveLink}
+                  to="auth"
+                  onClick={() => authCtx.logout()}
+                >
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          ) : (
+            <ul className="main-nav">
+              <li>
+                <NavLink style={styleActiveLink} to="/">
+                  Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink style={styleActiveLink} to="auth">
+                  Login or Register
+                </NavLink>
+              </li>
+            </ul>
+          )}
+        </nav>
+      </header>
+    );
 }
 
 export default Header
